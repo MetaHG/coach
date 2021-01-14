30f4d26f7d399c803f2efbfebcff2ba7b328fcad
Valentin Borgeaud <vborgeaud@logitech.com>
2021-01-14 16:15:32 +0000

    Fix compatibility with tensorflow < 1.14
    tf.compat.v1 is only available for tensorflow >= 1.14

--- rl_coach/__init__.py
+++ rl_coach/__init__.py
@@ -2,6 +2,6 @@
 import warnings
 warnings.filterwarnings('ignore',category=FutureWarning)
 
-# This gets rid of TF 2.0 related deprecation warnings
-import tensorflow as tf
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+# This gets rid of TF 2.0 related deprecation warnings ############ tf.compat.v1 is only compatible with tf >= 1.14. However, the requirements are still 1.9 <= tf <= 1.14
+# import tensorflow as tf
+# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
